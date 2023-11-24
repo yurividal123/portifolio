9066f0058594c2d406b5bf784bd25dd4475396b0
Yuri Vidal <yurividal123@hotmail.com>
2023-11-24 15:29:31 -0300

    stilizando o Sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './style'
+
+const Avatar = () => <FotoPerfil src="https://github.com/yurividal123.png" />
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,10 +1,23 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './style'
 
 const Sidebar = () => (
   <>
     <aside>
-      <img src="https://github.com/yurividal123.png" />
-      <Titulo fontSize={20}>Yuri Vidal</Titulo>
+      <SidebarContainer>
+        <Avatar />
+        <Titulo fontSize={20}>Yuri Vidal</Titulo>
+        <Paragrafo tipo="secundario" font-Size={16}>
+          yurividal123
+        </Paragrafo>
+        <Descricao tipo="principal" fontSize={12}>
+          Engenheiro Front-end
+        </Descricao>
+        <BotaoTema>Trocar tema</BotaoTema>
+      </SidebarContainer>
     </aside>
   </>
 )
