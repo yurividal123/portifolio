f824e97b9967954a8935b269c9f545559fd2dd57
Yuri Vidal <yurividal123@hotmail.com>
2023-11-23 21:21:55 -0300

    criando a seção de projetos e componentes de projeto

---
+++ src/components/Projeto/index.tsx
@@ -0,0 +1,13 @@
+import Paragrafo from '../Paragrafo'
+import Titulo from '../Titulo'
+import { Card, LinkBotao } from './style'
+
+const Projeto = () => (
+  <Card>
+    <Titulo>Projeto Lista de tarefas</Titulo>
+    <Paragrafo tipo="secundario">Lista de tarefas feita com VueJS</Paragrafo>
+    <LinkBotao>Visualizar</LinkBotao>
+  </Card>
+)
+
+export default Projeto
